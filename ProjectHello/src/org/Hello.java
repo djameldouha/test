b4d881267aadb5724a473efe0f17c8d178818a24
djameldouha <d.douha@univ-batna2.dz>
2021-08-24 20:01:54 +0200

    predator package there are an errors

--- ProjectHello/src/org/Hello.java
+++ ProjectHello/src/org/Hello.java
@@ -4,6 +4,7 @@ public class Hello {
 
 	public static void main(String[] args) {
 		String[] hellos= {"Salem","Bonjour","Hello","Hallo","Dia dhuit"};
+		
 		for(String h : hellos)
 		System.out.println(h);
 
---
+++ ProjectHello/src/predator/Doodlebugs.java
@@ -0,0 +1,19 @@
+package predator;
+
+public class Doodlebugs extends Organism{
+    
+    public Doodlebugs(){
+        super.setBody("X");
+    }
+
+    public void timeSteps(){
+        super.incTime();
+        if (getTime() == 8)
+            super.resetTime();
+    }
+
+    public void starve(){
+        if (super.getTime() == 3)
+            System.out.println("Hello");
+    }
+}
