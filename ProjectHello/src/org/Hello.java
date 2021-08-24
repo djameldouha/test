2bd7f3938662ccace5deb8d18c17a03098a5fc4e
djameldouha <d.douha@univ-batna2.dz>
2021-08-24 12:47:20 +0200

    add Italian hello

--- ProjectHello/src/org/Hello.java
+++ ProjectHello/src/org/Hello.java
@@ -3,7 +3,7 @@ package org;
 public class Hello {
 
 	public static void main(String[] args) {
-		String[] hellos= {"Salem","Bonjour","Hello","Hallo"};//Dia dhuit. 
+		String[] hellos= {"Salem","Bonjour","Hello","Hallo","Dia dhuit"};
 		for(String h : hellos)
 		System.out.println(h);
 
